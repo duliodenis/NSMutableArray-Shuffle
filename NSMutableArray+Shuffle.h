588e4e8f98de939ba378a7056ce949bea68e95fd
kokoloco64 <dulio.denis@yahoo.com>
2014-01-13 16:05:25 -0500

    Initial commit

---
+++ NSMutableArray+Shuffle.h
@@ -0,0 +1,15 @@
+//
+//  NSMutableArray+Shuffle.h
+//  A helpful category on NSMutableArray that provides a method to shuffle the array.
+//
+//  Created by Dulio Denis on 11/8/13.
+//  Copyright (c) 2013 ddApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (Shuffle)
+
+- (void)shuffle;
+
+@end
---
+++ NSMutableArray+Shuffle.m
@@ -0,0 +1,24 @@
+//
+//  NSMutableArray+Shuffle.m
+//  A helpful category on NSMutableArray that provides a method to shuffle the array.
+//
+//  Created by Dulio Denis on 11/8/13.
+//  Copyright (c) 2013 ddApps. All rights reserved.
+//
+
+#import "NSMutableArray+Shuffle.h"
+
+@implementation NSMutableArray (Shuffle)
+
+- (void)shuffle
+{
+    NSUInteger count = [self count];
+    for (NSUInteger i = 0; i < count; ++i) {
+        // Select a random element between i and end of array to swap with.
+        NSInteger nElements = count - i;
+        NSInteger n = arc4random_uniform(nElements) + i;
+        [self exchangeObjectAtIndex:i withObjectAtIndex:n];
+    }
+}
+
+@end
